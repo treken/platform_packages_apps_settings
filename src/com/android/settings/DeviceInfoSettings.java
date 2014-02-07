d69bc3df706c76b7df028f5955e9352f463fb5df
treken <chupekevin@gmail.com>
2014-02-06 20:24:05 -0500

    OctOS Easter Egg(2/2)
    Thanks to CarbonDev for their easter egg commit!
    Also removed obsolete directory "oct", it was missed from moving Quick Tiles to Tentacles
    
    Conflicts:
    	src/com/android/settings/DeviceInfoSettings.java
    
    Change-Id: I3d28e5a81efca83b946c514dbb28b321a8660f68

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -80,6 +80,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
     long[] mHits = new long[3];
+    long[] mTaps = new long[3];
     int mDevHitCountdown;
     Toast mDevHitToast;
 
@@ -210,6 +211,19 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
                 Intent intent = new Intent(Intent.ACTION_MAIN);
                 intent.setClassName("android",
                         com.android.internal.app.PlatLogoActivity.class.getName());
+                try {
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Unable to start activity " + intent.toString());
+                }
+            }
+        } else if (preference.getKey().equals(KEY_KERNEL_VERSION)) {
+            System.arraycopy(mTaps, 1, mTaps, 0, mTaps.length-1);
+            mTaps[mTaps.length-1] = SystemClock.uptimeMillis();
+            if (mTaps[0] >= (SystemClock.uptimeMillis()-500)) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.setClassName("android",
+                        com.android.internal.app.OctLogoActivity.class.getName());
                 try {
                     startActivity(intent);
                 } catch (Exception e) {
